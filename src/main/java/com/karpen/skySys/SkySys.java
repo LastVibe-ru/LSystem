94517e60da25fa922217e21ec445617ca9783b36
karpen-dev <ghhvvfghvg44@gmail.com>
2025-03-24 10:28:57 +0300

    add restart timer and head droping

--- src/main/java/com/karpen/skySys/SkySys.java
+++ src/main/java/com/karpen/skySys/SkySys.java
@@ -2,21 +2,29 @@ package com.karpen.skySys;
 
 import com.maximde.entitysize.EntityModifierService;
 import com.maximde.entitysize.EntitySize;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
+import org.bukkit.*;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.*;
 
-public final class SkySys extends JavaPlugin implements TabCompleter {
+public final class SkySys extends JavaPlugin implements TabCompleter, Listener {
 
     private final Map<Player, ItemStack[]> inv = new HashMap<>();
 
@@ -62,6 +70,12 @@ public final class SkySys extends JavaPlugin implements TabCompleter {
     private void restoreInv(Player player){
         if (inv.containsKey(player)){
             player.getInventory().setContents(inv.get(player));
+        }
+    }
+
+    private void playSoundAllPlayers(){
+        for (Player player : Bukkit.getOnlinePlayers()){
+            player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1.0f, 1.0f);
         }
     }
 
@@ -157,17 +171,56 @@ public final class SkySys extends JavaPlugin implements TabCompleter {
             }
         }
 
-        if (command.getName().equalsIgnoreCase("ncolor")){
-            if (!(sender instanceof Player)){
-                sender.sendMessage(ChatColor.RED + "Эту команду может юзать только игрок");
+        if (command.getName().equalsIgnoreCase("vrestart")) {
+            int countdownTime = 60;
+            BossBar bar = Bukkit.createBossBar("Рестарт через:", BarColor.WHITE, BarStyle.SOLID);
+            bar.setVisible(true);
 
-                return true;
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                bar.addPlayer(player);
             }
 
-            Player player = (Player) sender;
+            new BukkitRunnable() {
+                int remainingTime = countdownTime;
 
+                @Override
+                public void run() {
+                    if (remainingTime > 0) {
+                        bar.setProgress((double) remainingTime / countdownTime);
+                        bar.setTitle("Рестарт через: " + remainingTime);
 
+                        if (remainingTime == 30) {
+                            Bukkit.broadcastMessage(ChatColor.WHITE + "Рестарт через 30 секунд");
+                        } else if (remainingTime == 10) {
+                            Bukkit.broadcastMessage(ChatColor.WHITE + "Рестарт через 10 секунд");
+                        } else if (remainingTime == 5) {
+                            Bukkit.broadcastMessage(ChatColor.WHITE + "Рестарт через 5 секунд");
+                        } else if (remainingTime == 3) {
+                            playSoundAllPlayers();
+                            Bukkit.broadcastMessage(ChatColor.WHITE + "Рестарт через 3 секунды");
+                        } else if (remainingTime == 2) {
+                            playSoundAllPlayers();
+                            Bukkit.broadcastMessage(ChatColor.WHITE + "Рестарт через 2 секунды");
+                        } else if (remainingTime == 1) {
+                            playSoundAllPlayers();
+                            Bukkit.broadcastMessage(ChatColor.WHITE + "Рестарт через 1 секунду");
+                        }
+
+                        remainingTime--;
+                    } else {
+                        Bukkit.broadcastMessage(ChatColor.WHITE + "Рестарт");
+                        bar.setVisible(false);
+                        bar.removeAll();
+
+                        Bukkit.getServer().shutdown();
+                        cancel();
+                    }
+                }
+            }.runTaskTimer(this, 0, 20);
+
+            return true;
         }
+
 
         return false;
     }
@@ -191,5 +244,32 @@ public final class SkySys extends JavaPlugin implements TabCompleter {
         }
 
         return suggestions;
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event){
+        if (event.getEntity().getKiller() == null){
+            return;
+        }
+
+        Player player = (Player) event.getEntity();
+        Player killer = (Player) event.getEntity().getKiller();
+
+        assert killer != null;
+        ItemStack item = killer.getInventory().getItemInMainHand();
+
+        if (item.equals(Material.WOODEN_AXE) || item.equals(Material.STONE_AXE) || item.equals(Material.IRON_AXE) || item.equals(Material.GOLDEN_AXE) || item.equals(Material.DIAMOND_AXE) || item.equals(Material.NETHERITE_AXE)){
+            Location location = player.getLocation();
+
+            ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+            SkullMeta meta = (SkullMeta) head.getItemMeta();
+
+            if (meta != null){
+                meta.setOwningPlayer(player);
+                head.setItemMeta(meta);
+            }
+
+            Objects.requireNonNull(location.getWorld()).dropItem(location, head);
+        }
     }
 }
